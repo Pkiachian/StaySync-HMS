062735583d13a49a928e3d8919e920ecad5532bd
Pkiachian <pkiachian7@gmail.com>
2026-05-14 08:16:26 +0300

    Add AuthController boilerplate

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Handle user login
+     */
+    public function login(Request $request)
+    {
+        // TODO: Implement login logic
+    }
+
+    /**
+     * Handle user logout
+     */
+    public function logout(Request $request)
+    {
+        // TODO: Implement logout logic
+    }
+
+    /**
+     * Handle user registration
+     */
+    public function register(Request $request)
+    {
+        // TODO: Implement registration logic
+    }
+}
