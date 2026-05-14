0e1b01d9699499b5abd97df4a464a8f57d6c57b0
Pkiachian <pkiachian7@gmail.com>
2026-05-14 08:17:38 +0300

    Add BookingController boilerplate

---
+++ backend/app/Http/Controllers/BookingController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    /**
+     * Get all bookings
+     */
+    public function index()
+    {
+        // TODO: Implement get all bookings
+    }
+
+    /**
+     * Create a new booking
+     */
+    public function store(Request $request)
+    {
+        // TODO: Implement create booking
+    }
+
+    /**
+     * Get booking details
+     */
+    public function show($id)
+    {
+        // TODO: Implement get booking details
+    }
+
+    /**
+     * Update booking
+     */
+    public function update(Request $request, $id)
+    {
+        // TODO: Implement update booking
+    }
+
+    /**
+     * Cancel booking
+     */
+    public function destroy($id)
+    {
+        // TODO: Implement cancel booking
+    }
+}
