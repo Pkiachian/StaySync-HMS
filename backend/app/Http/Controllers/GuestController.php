3722b77837fe000567cf14c13f4d4f6d4822aa65
Pkiachian <pkiachian7@gmail.com>
2026-05-14 09:00:57 +0300

    Add GuestController

---
+++ backend/app/Http/Controllers/GuestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GuestController extends Controller
+{
+    /**
+     * Get all guests
+     */
+    public function index()
+    {
+        // TODO: Implement get all guests
+    }
+
+    /**
+     * Create a new guest
+     */
+    public function store(Request $request)
+    {
+        // TODO: Implement create guest
+    }
+
+    /**
+     * Get guest details
+     */
+    public function show($id)
+    {
+        // TODO: Implement get guest details
+    }
+
+    /**
+     * Update guest
+     */
+    public function update(Request $request, $id)
+    {
+        // TODO: Implement update guest
+    }
+
+    /**
+     * Delete guest
+     */
+    public function destroy($id)
+    {
+        // TODO: Implement delete guest
+    }
+}
