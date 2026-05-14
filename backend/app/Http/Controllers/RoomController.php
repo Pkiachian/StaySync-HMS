69a7c3255a2ba1ad1d550968b4a81f3461472bb9
Pkiachian <pkiachian7@gmail.com>
2026-05-14 08:59:16 +0300

    Add RoomController

---
+++ backend/app/Http/Controllers/RoomController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Get all rooms
+     */
+    public function index()
+    {
+        // TODO: Implement get all rooms
+    }
+
+    /**
+     * Create a new room
+     */
+    public function store(Request $request)
+    {
+        // TODO: Implement create room
+    }
+
+    /**
+     * Get room details
+     */
+    public function show($id)
+    {
+        // TODO: Implement get room details
+    }
+
+    /**
+     * Update room
+     */
+    public function update(Request $request, $id)
+    {
+        // TODO: Implement update room
+    }
+
+    /**
+     * Delete room
+     */
+    public function destroy($id)
+    {
+        // TODO: Implement delete room
+    }
+}
